d8377c680b615adc2d2eb0980c0dfbf061c28aa9
Eleanor McHugh <eleanor@games-with-brains.com>
2011-07-02 23:58:10 +0100

    Added SSlice type for storing strings

---
+++ string.go
@@ -0,0 +1,330 @@
+package slices
+
+import "fmt"
+
+func SList(n... string) *SSlice {
+	return (*SSlice)(&n)
+}
+
+type SSlice	[]string
+
+func (s SSlice) Len() int							{ return len(s) }
+func (s SSlice) Cap() int							{ return cap(s) }
+
+func (s SSlice) At(i int) interface{}				{ return s[i] }
+func (s SSlice) SAt(i int) string					{ return s[i] }
+func (s SSlice) Set(i int, v interface{})			{ s[i] = v.(string) }
+func (s SSlice) SSet(i int, v string)				{ s[i] = v }
+func (s SSlice) Clear(i int)						{ s[i] = "" }
+func (s SSlice) Swap(i, j int)						{ s[i], s[j] = s[j], s[i] }
+
+func (s SSlice) Add(i, j int)						{ s[i] += s[j] }
+
+func (s SSlice) Less(i, j int) bool					{ return s[i] < s[j] }
+func (s SSlice) AtLeast(i, j int) bool				{ return s[i] <= s[j] }
+func (s SSlice) Same(i, j int) bool					{ return s[i] == s[j] }
+func (s SSlice) AtMost(i, j int) bool				{ return s[i] >= s[j] }
+func (s SSlice) More(i, j int) bool					{ return s[i] > s[j] }
+
+func (s *SSlice) Slice(i, j int)					{ *s = (*s)[i:j] }
+
+func (s SSlice) Compare(i, j int) (r int) {
+	switch {
+	case s[i] < s[j]:		r = IS_LESS_THAN
+	case s[i] > s[j]:		r = IS_GREATER_THAN
+	default:				r = IS_SAME_AS
+	}
+	return
+}
+
+func (s *SSlice) Cut(i, j int) {
+	a := *s
+	l := len(a)
+	if i < 0 {
+		i = 0
+	}
+	if j > l {
+		j = l
+	}
+	if j > i {
+		if m := l - (j - i); m > 0 && l > m {
+			copy(a[i:m], a[j:l])
+			*s = a[0:m]
+		}
+	}
+}
+
+func (s *SSlice) Trim(i, j int) {
+	a := *s
+	n := len(a)
+	if i < 0 {
+		i = 0
+	}
+	if j > n {
+		j = n
+	}
+	if j > i {
+		copy(a, a[i:j])
+		*s = a[0:j - i]
+	}
+}
+
+func (s *SSlice) Delete(i int) {
+	a := *s
+	n := len(a)
+	if i > -1 && i < n {
+		copy(a[i:n - 1], a[i + 1:n])
+		*s = a[0 : n - 1]
+	}
+}
+
+func (s SSlice) Each(f func(interface{})) {
+	for _, v := range s {
+		f(v)
+	}
+}
+
+func (s SSlice) EachWithIndex(f func(int, interface{})) {
+	for i, v := range s {
+		f(i, v)
+	}
+}
+
+func (s SSlice) EachWithKey(f func(key, value interface{})) {
+	for i, v := range s {
+		f(i, v)
+	}
+}
+
+func (s SSlice) SEach(f func(string)) {
+	for _, v := range s {
+		f(v)
+	}
+}
+
+func (s SSlice) SEachWithIndex(f func(int, string)) {
+	for i, v := range s {
+		f(i, v)
+	}
+}
+
+func (s SSlice) SEachWithKey(f func(interface{}, string)) {
+	for i, v := range s {
+		f(i, v)
+	}
+}
+
+func (s SSlice) String() (t string) {
+	for _, v := range s {
+		if len(t) > 0 {
+			t += " "
+		}
+		t += fmt.Sprintf("%v", v)
+	}
+	return fmt.Sprintf("(%v)", t)
+}
+
+func (s SSlice) BlockCopy(destination, source, count int) {
+	end := source + count
+	if end > len(s) {
+		end = len(s)
+	}
+	copy(s[destination:], s[source:end])
+}
+
+func (s SSlice) BlockClear(start, count int) {
+	copy(s[start:], make(SSlice, count, count))
+}
+
+func (s SSlice) Overwrite(offset int, container interface{}) {
+	switch container := container.(type) {
+	case SSlice:			copy(s[offset:], container)
+	case []string:			copy(s[offset:], container)
+	}
+}
+
+func (s *SSlice) Reallocate(length, capacity int) {
+	switch {
+	case length > capacity:		s.Reallocate(capacity, capacity)
+	case capacity != cap(*s):	x := make(SSlice, length, capacity)
+								copy(x, *s)
+								*s = x
+	default:					*s = (*s)[:length]
+	}
+}
+
+func (s *SSlice) Extend(n int) {
+	c := cap(*s)
+	l := len(*s) + n
+	if l > c {
+		c = l
+	}
+	s.Reallocate(l, c)
+}
+
+func (s *SSlice) Expand(i, n int) {
+	if i < 0 {
+		i = 0
+	}
+
+	l := s.Len()
+	if l < i {
+		i = l
+	}
+
+	l += n
+	c := s.Cap()
+	if c < l {
+		c = l
+	}
+
+	if c != s.Cap() {
+		x := make(SSlice, l, c)
+		copy(x, (*s)[:i])
+		copy(x[i + n:], (*s)[i:])
+		*s = x
+	} else {
+		a := (*s)[:l]
+		for j := l - 1; j >= i; j-- {
+			a[j] = a[j - n]
+		}
+		*s = a
+	}
+}
+
+func (s SSlice) Reverse() {
+	end := s.Len() - 1
+	for i := 0; i < end; i++ {
+		s[i], s[end] = s[end], s[i]
+		end--
+	}
+}
+
+func (s SSlice) Depth() int {
+	return 0
+}
+
+func (s *SSlice) Append(v interface{}) {
+	s.SAppend(v.(string))
+}
+
+func (s *SSlice) SAppend(v string) {
+	*s = append(*s, v)
+}
+
+func (s *SSlice) AppendSlice(o SSlice) {
+	*s = append(*s, o...)
+}
+
+func (s *SSlice) Prepend(v interface{}) {
+	s.SPrepend(v.(string))
+}
+
+func (s *SSlice) SPrepend(v string) {
+	l := s.Len() + 1
+	n := make(SSlice, l, l)
+	n[0] = v
+	copy(n[1:], *s)
+	*s = n
+}
+
+func (s *SSlice) PrependSlice(o SSlice) {
+	l := s.Len() + o.Len()
+	n := make(SSlice, l, l)
+	copy(n, o)
+	copy(n[o.Len():], *s)
+	*s = n
+}
+
+func (s SSlice) Repeat(count int) SSlice {
+	length := len(s) * count
+	capacity := cap(s)
+	if capacity < length {
+		capacity = length
+	}
+	destination := make(SSlice, length, capacity)
+	for start, end := 0, len(s); count > 0; count-- {
+		copy(destination[start:end], s)
+		start = end
+		end += len(s)
+	}
+	return destination
+}
+
+func (s *SSlice) Flatten() {
+	//	Flatten is a non-op for the SSlice as they cannot contain nested elements
+}
+
+func (s SSlice) equal(o SSlice) (r bool) {
+	switch {
+	case s == nil:				r = o == nil
+	case s.Len() == o.Len():	r = true
+								for i, v := range s {
+									if r = v == o[i]; !r {
+										return
+									}
+								}
+	}
+	return
+}
+
+func (s SSlice) Equal(o interface{}) (r bool) {
+	switch o := o.(type) {
+	case *SSlice:			r = o != nil && s.equal(*o)
+	case SSlice:			r = s.equal(o)
+	case *[]string:			r = o != nil && s.equal(*o)
+	case []string:			r = s.equal(o)
+	}
+	return
+}
+
+func (s SSlice) Car() (h interface{}) {
+	if s.Len() > 0 {
+		h = s[0]
+	}
+	return
+}
+
+func (s SSlice) Cdr() (t SSlice) {
+	if s.Len() > 1 {
+		t = s[1:]
+	}
+	return
+}
+
+func (s *SSlice) Rplaca(v interface{}) {
+	switch {
+	case s == nil:			*s = *SList(v.(string))
+	case s.Len() == 0:		*s = append(*s, v.(string))
+	default:				(*s)[0] = v.(string)
+	}
+}
+
+func (s *SSlice) Rplacd(v interface{}) {
+	if s == nil {
+		*s = *SList(v.(string))
+	} else {
+		ReplaceSlice := func(v SSlice) {
+			if l := len(v); l < cap(*s) {
+				copy((*s)[1:], v)
+				*s = (*s)[0:l + 1]
+			} else {
+				l++
+				n := make(SSlice, l, l)
+				copy(n, (*s)[:1])
+				copy(n[1:], v)
+				*s = n
+			}
+		}
+
+		switch v := v.(type) {
+		case *SSlice:		ReplaceSlice(*v)
+		case SSlice:		ReplaceSlice(v)
+		case *[]string:		ReplaceSlice(SSlice(*v))
+		case []string:		ReplaceSlice(SSlice(v))
+		case nil:			*s = (*s)[:1]
+		default:			(*s)[1] = v.(string)
+							*s = (*s)[:2]
+		}
+	}
+}
---
+++ string_test.go
@@ -0,0 +1,414 @@
+package slices
+
+import "testing"
+
+func TestSSlice(t *testing.T) {
+	sxp := SList("A")
+	switch {
+	case sxp.Len() != 1:			t.Fatalf("SList(A) should allocate 1 cells, not %v cells", sxp.Len())
+	case sxp.At(0) != "A":			t.Fatalf("SList(A) element 0 should be A and not %v", sxp.At(0))
+	}
+
+	sxp = SList("A", "B")
+	switch {
+	case sxp.Len() != 2:			t.Fatalf("SList(A B) should allocate 2 cells, not %v cells", sxp.Len())
+	case sxp.At(0) != "A":			t.Fatalf("SList(A B) element 0 should be A and not %v", sxp.At(0))
+	case sxp.At(1) != "B":			t.Fatalf("SList(A B) element 1 should be B and not %v", sxp.At(1))
+	}
+
+	sxp = SList("A", "B", "C")
+	switch {
+	case sxp.Len() != 3:			t.Fatalf("SList(A B C) should allocate 3 cells, not %v cells", sxp.Len())
+	case sxp.At(0) != "A":			t.Fatalf("SList(A B C) element 0 should be A and not %v", sxp.At(0))
+	case sxp.At(1) != "B":			t.Fatalf("SList(A B C) element 1 should be B and not %v", sxp.At(1))
+	case sxp.At(2) != "C":			t.Fatalf("SList(A B C) element 2 should be C and not %v", sxp.At(2))
+	}
+}
+
+func TestSSliceString(t *testing.T) {
+	ConfirmString := func(s *SSlice, r string) {
+		if x := s.String(); x != r {
+			t.Fatalf("%v erroneously serialised as '%v'", r, x)
+		}
+	}
+
+	ConfirmString(SList(), "()")
+	ConfirmString(SList("A"), "(A)")
+	ConfirmString(SList("A", "B"), "(A B)")
+}
+
+func TestSSliceLen(t *testing.T) {
+	ConfirmLength := func(s *SSlice, i int) {
+		if x := s.Len(); x != i {
+			t.Fatalf("%v.Len() should be %v but is %v", s, i, x)
+		}
+	}
+	
+	ConfirmLength(SList("A"), 1)
+	ConfirmLength(SList("A", "B"), 2)
+}
+
+func TestSSliceSwap(t *testing.T) {
+	ConfirmSwap := func(s *SSlice, i, j int, r *SSlice) {
+		if s.Swap(i, j); !r.Equal(s) {
+			t.Fatalf("Swap(%v, %v) should be %v but is %v", i, j, r, s)
+		}
+	}
+	ConfirmSwap(SList("A", "B", "C"), 0, 1, SList("B", "A", "C"))
+	ConfirmSwap(SList("A", "B", "C"), 0, 2, SList("C", "B", "A"))
+}
+
+func TestSSliceCompare(t *testing.T) {
+	ConfirmCompare := func(s *SSlice, i, j, r int) {
+		if x := s.Compare(i, j); x != r {
+			t.Fatalf("Compare(%v, %v) should be %v but is %v", i, j, r, x)
+		}
+	}
+
+	ConfirmCompare(SList("A", "B"), 0, 0, IS_SAME_AS)
+	ConfirmCompare(SList("A", "B"), 0, 1, IS_LESS_THAN)
+	ConfirmCompare(SList("A", "B"), 1, 0, IS_GREATER_THAN)
+}
+
+func TestSSliceCut(t *testing.T) {
+	ConfirmCut := func(s *SSlice, start, end int, r *SSlice) {
+		if s.Cut(start, end); !r.Equal(s) {
+			t.Fatalf("Cut(%v, %v) should be %v but is %v", start, end, r, s)
+		}
+	}
+
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 0, 1, SList("B", "C", "D", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 1, 2, SList("A", "C", "D", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 2, 3, SList("A", "B", "D", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 3, 4, SList("A", "B", "C", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 4, 5, SList("A", "B", "C", "D", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 5, 6, SList("A", "B", "C", "D", "E"))
+
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), -1, 1, SList("B", "C", "D", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 0, 2, SList("C", "D", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 1, 3, SList("A", "D", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 2, 4, SList("A", "B", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 3, 5, SList("A", "B", "C", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 4, 6, SList("A", "B", "C", "D"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 5, 7, SList("A", "B", "C", "D", "E"))
+}
+
+func TestSSliceTrim(t *testing.T) {
+	ConfirmTrim := func(s *SSlice, start, end int, r *SSlice) {
+		if s.Trim(start, end); !r.Equal(s) {
+			t.Fatalf("Cut(%v, %v) should be %v but is %v", start, end, r, s)
+		}
+	}
+
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 0, 1, SList("A"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 1, 2, SList("B"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 2, 3, SList("C"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 3, 4, SList("D"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 4, 5, SList("E"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 5, 6, SList("F"))
+
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), -1, 1, SList("A"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 0, 2, SList("A", "B"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 1, 3, SList("B", "C"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 2, 4, SList("C", "D"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 3, 5, SList("D", "E"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 4, 6, SList("E", "F"))
+	ConfirmTrim(SList("A", "B", "C", "D", "E", "F"), 5, 7, SList("F"))
+}
+
+func TestSSliceDelete(t *testing.T) {
+	ConfirmCut := func(s *SSlice, index int, r *SSlice) {
+		if s.Delete(index); !r.Equal(s) {
+			t.Fatalf("Delete(%v) should be %v but is %v", index, r, s)
+		}
+	}
+
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), -1, SList("A", "B", "C", "D", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 0, SList("B", "C", "D", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 1, SList("A", "C", "D", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 2, SList("A", "B", "D", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 3, SList("A", "B", "C", "E", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 4, SList("A", "B", "C", "D", "F"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 5, SList("A", "B", "C", "D", "E"))
+	ConfirmCut(SList("A", "B", "C", "D", "E", "F"), 6, SList("A", "B", "C", "D", "E", "F"))
+}
+
+func TestSSliceEach(t *testing.T) {
+	c := SList("A", "B", "C", "D", "E", "F")
+	count := 0
+	c.Each(func(i interface{}) {
+		if i != string([]byte{ byte(count) + "A"[0] }) {
+			t.Fatalf("element %v erroneously reported as %v", count, i)
+		}
+		count++
+	})
+}
+
+func TestSSliceEachWithIndex(t *testing.T) {
+	c := SList("A", "B", "C", "D", "E", "F")
+	c.EachWithIndex(func(index int, i interface{}) {
+		if i != string([]byte{ byte(index) + "A"[0] }) {
+			t.Fatalf("element %v erroneously reported as %v", index, i)
+		}
+	})
+}
+
+func TestSSliceEachWithKey(t *testing.T) {
+	c := SList("A", "B", "C", "D", "E", "F")
+	c.EachWithKey(func(key, i interface{}) {
+		if i != string([]byte{ byte(key.(int)) + "A"[0] }) {
+			t.Fatalf("element %v erroneously reported as %v", key, i)
+		}
+	})
+}
+
+func TestSSliceIEach(t *testing.T) {
+	c := SList("A", "B", "C", "D", "E", "F")
+	count := 0
+	c.SEach(func(i string) {
+		if i != string([]byte{ byte(count) + "A"[0] }) {
+			t.Fatalf("element %v erroneously reported as %v", count, i)
+		}
+		count++
+	})
+}
+
+func TestSSliceIEachWithIndex(t *testing.T) {
+	c := SList("A", "B", "C", "D", "E", "F")
+	c.SEachWithIndex(func(index int, i string) {
+		if i != string([]byte{ byte(index) + "A"[0] }) {
+			t.Fatalf("element %v erroneously reported as %v", index, i)
+		}
+	})
+}
+
+func TestSSliceIEachWithKey(t *testing.T) {
+	c := SList("A", "B", "C", "D", "E", "F")
+	c.SEachWithKey(func(key interface{}, i string) {
+		if i != string([]byte{ byte(key.(int)) + "A"[0] }) {
+			t.Fatalf("element %v erroneously reported as %v", key, i)
+		}
+	})
+}
+
+func TestSSliceBlockCopy(t *testing.T) {
+	ConfirmBlockCopy := func(s *SSlice, destination, source, count int, r *SSlice) {
+		s.BlockCopy(destination, source, count)
+		if !r.Equal(s) {
+			t.Fatalf("BlockCopy(%v, %v, %v) should be %v but is %v", destination, source, count, r, s)
+		}
+	}
+
+	ConfirmBlockCopy(SList("A", "B", "C", "D", "E", "F"), 0, 0, 4, SList("A", "B", "C", "D", "E", "F"))
+	ConfirmBlockCopy(SList("A", "B", "C", "D", "E", "F"), 6, 6, 4, SList("A", "B", "C", "D", "E", "F"))
+	ConfirmBlockCopy(SList("A", "B", "C", "D", "E", "F"), 4, 2, 2, SList("A", "B", "C", "D", "C", "D"))
+	ConfirmBlockCopy(SList("A", "B", "C", "D", "E", "F"), 2, 4, 4, SList("A", "B", "E", "F", "E", "F"))
+}
+
+func TestSSliceBlockClear(t *testing.T) {
+	ConfirmBlockClear := func(s *SSlice, start, count int, r *SSlice) {
+		s.BlockClear(start, count)
+		if !r.Equal(s) {
+			t.Fatalf("BlockClear(%v, %v) should be %v but is %v", start, count, r, s)
+		}
+	}
+
+	ConfirmBlockClear(SList("A", "B", "C", "D", "E", "F"), 0, 4, SList("", "", "", "", "E", "F"))
+	ConfirmBlockClear(SList("A", "B", "C", "D", "E", "F"), 1, 4, SList("A", "", "", "", "", "F"))
+	ConfirmBlockClear(SList("A", "B", "C", "D", "E", "F"), 2, 4, SList("A", "B", "", "", "", ""))
+}
+
+func TestSSliceOverwrite(t *testing.T) {
+	ConfirmOverwrite := func(s *SSlice, offset int, v, r *SSlice) {
+		s.Overwrite(offset, *v)
+		if !r.Equal(s) {
+			t.Fatalf("Overwrite(%v, %v) should be %v but is %v", offset, v, r, s)
+		}
+	}
+
+	ConfirmOverwrite(SList("A", "B", "C", "D", "E", "F"), 0, SList("Z", "Y", "X"), SList("Z", "Y", "X", "D", "E", "F"))
+	ConfirmOverwrite(SList("A", "B", "C", "D", "E", "F"), 6, SList("Z", "Y", "X"), SList("A", "B", "C", "D", "E", "F"))
+	ConfirmOverwrite(SList("A", "B", "C", "D", "E", "F"), 2, SList("Z", "Y", "X"), SList("A", "B", "Z", "Y", "X", "F"))
+}
+
+func TestSSliceReallocate(t *testing.T) {
+	ConfirmReallocate := func(s *SSlice, l, c int, r *SSlice) {
+		o := s.String()
+		el := l
+		if el > c {
+			el = c
+		}
+		switch s.Reallocate(l, c); {
+		case s == nil:				t.Fatalf("%v.Reallocate(%v, %v) created a nil value for Slice", o, l, c)
+		case s.Cap() != c:			t.Fatalf("%v.Reallocate(%v, %v) capacity should be %v but is %v", o, l, c, c, s.Cap())
+		case s.Len() != el:			t.Fatalf("%v.Reallocate(%v, %v) length should be %v but is %v", o, l, c, el, s.Len())
+		case !r.Equal(s):			t.Fatalf("%v.Reallocate(%v, %v) should be %v but is %v", o, l, c, r, s)
+		}
+	}
+
+	ConfirmReallocate(SList(), 0, 10, SList())
+	ConfirmReallocate(SList("A", "B", "C", "D", "E", "F"), 3, 10, SList("A", "B", "C"))
+	ConfirmReallocate(SList("A", "B", "C", "D", "E", "F"), 6, 10, SList("A", "B", "C", "D", "E", "F"))
+	ConfirmReallocate(SList("A", "B", "C", "D", "E", "F"), 10, 10, SList("A", "B", "C", "D", "E", "F", "", "", "", ""))
+	ConfirmReallocate(SList("A", "B", "C", "D", "E", "F"), 1, 3, SList("A"))
+	ConfirmReallocate(SList("A", "B", "C", "D", "E", "F"), 3, 3, SList("A", "B", "C"))
+	ConfirmReallocate(SList("A", "B", "C", "D", "E", "F"), 6, 3, SList("A", "B", "C"))
+}
+
+func TestSSliceExtend(t *testing.T) {
+	ConfirmExtend := func(s *SSlice, n int, r *SSlice) {
+		c := s.Cap()
+		s.Extend(n)
+		switch {
+		case s.Len() != r.Len():	t.Fatalf("Extend(%v) len should be %v but is %v", n, r.Len(), s.Len())
+		case s.Cap() != c + n:		t.Fatalf("Extend(%v) cap should be %v but is %v", n, c + n, s.Cap())
+		case !r.Equal(s):			t.Fatalf("Extend(%v) should be %v but is %v", n, r, s)
+		}
+	}
+
+	ConfirmExtend(SList(), 1, SList(""))
+	ConfirmExtend(SList(), 2, SList("", ""))
+}
+
+func TestSSliceExpand(t *testing.T) {
+	ConfirmExpand := func(s *SSlice, i, n int, r *SSlice) {
+		c := s.Cap()
+		s.Expand(i, n)
+		switch {
+		case s.Len() != r.Len():	t.Fatalf("Expand(%v, %v) len should be %v but is %v", i, n, r.Len(), s.Len())
+		case s.Cap() != c + n:		t.Fatalf("Expand(%v, %v) cap should be %v but is %v", i, n, c + n, s.Cap())
+		case !r.Equal(s):			t.Fatalf("Expand(%v, %v) should be %v but is %v", i, n, r, s)
+		}
+	}
+
+	ConfirmExpand(SList(), -1, 1, SList(""))
+	ConfirmExpand(SList(), 0, 1, SList(""))
+	ConfirmExpand(SList(), 1, 1, SList(""))
+	ConfirmExpand(SList(), 0, 2, SList("", ""))
+
+	ConfirmExpand(SList("A", "B", "C"), -1, 2, SList("", "", "A", "B", "C"))
+	ConfirmExpand(SList("A", "B", "C"), 0, 2, SList("", "", "A", "B", "C"))
+	ConfirmExpand(SList("A", "B", "C"), 1, 2, SList("A", "", "", "B", "C"))
+	ConfirmExpand(SList("A", "B", "C"), 2, 2, SList("A", "B", "", "", "C"))
+	ConfirmExpand(SList("A", "B", "C"), 3, 2, SList("A", "B", "C", "", ""))
+	ConfirmExpand(SList("A", "B", "C"), 4, 2, SList("A", "B", "C", "", ""))
+}
+
+func TestSSliceDepth(t *testing.T) {
+	ConfirmDepth := func(s *SSlice, i int) {
+		if x := s.Depth(); x != i {
+			t.Fatalf("%v.Depth() should be %v but is %v", s, i, x)
+		}
+	}
+	ConfirmDepth(SList("A", "B"), 0)
+}
+
+func TestSSliceReverse(t *testing.T) {
+	sxp := SList("A", "B", "C", "D", "E", "F")
+	rxp := SList("F", "E", "D", "C", "B", "A")
+	sxp.Reverse()
+	if !rxp.Equal(sxp) {
+		t.Fatalf("Reversal failed: %v", sxp)
+	}
+}
+
+func TestSSliceAppend(t *testing.T) {
+	ConfirmAppend := func(s *SSlice, v interface{}, r *SSlice) {
+		s.Append(v)
+		if !r.Equal(s) {
+			t.Fatalf("Append(%v) should be %v but is %v", v, r, s)
+		}
+	}
+
+	ConfirmAppend(SList(), "A", SList("A"))
+}
+
+func TestSSliceAppendSlice(t *testing.T) {
+	ConfirmAppendSlice := func(s, v, r *SSlice) {
+		s.AppendSlice(*v)
+		if !r.Equal(s) {
+			t.Fatalf("AppendSlice(%v) should be %v but is %v", v, r, s)
+		}
+	}
+
+	ConfirmAppendSlice(SList(), SList("A"), SList("A"))
+	ConfirmAppendSlice(SList(), SList("A", "B"), SList("A", "B"))
+	ConfirmAppendSlice(SList("A", "B", "C"), SList("D", "E", "F"), SList("A", "B", "C", "D", "E", "F"))
+}
+
+func TestSSlicePrepend(t *testing.T) {
+	ConfirmPrepend := func(s *SSlice, v interface{}, r *SSlice) {
+		if s.Prepend(v); !r.Equal(s) {
+			t.Fatalf("Prepend(%v) should be %v but is %v", v, r, s)
+		}
+	}
+
+	ConfirmPrepend(SList(), "A", SList("A"))
+	ConfirmPrepend(SList("A"), "B", SList("B", "A"))
+}
+
+func TestSSlicePrependSlice(t *testing.T) {
+	ConfirmPrependSlice := func(s, v, r *SSlice) {
+		if s.PrependSlice(*v); !r.Equal(s) {
+			t.Fatalf("PrependSlice(%v) should be %v but is %v", v, r, s)
+		}
+	}
+
+	ConfirmPrependSlice(SList(), SList("A"), SList("A"))
+	ConfirmPrependSlice(SList(), SList("A", "B"), SList("A", "B"))
+	ConfirmPrependSlice(SList("A", "B", "C"), SList("D", "E", "F"), SList("D", "E", "F", "A", "B", "C"))
+}
+
+func TestSSliceRepeat(t *testing.T) {
+	ConfirmRepeat := func(s *SSlice, count int, r *SSlice) {
+		if x := s.Repeat(count); !x.Equal(r) {
+			t.Fatalf("%v.Repeat(%v) should be %v but is %v", s, count, r, x)
+		}
+	}
+
+	ConfirmRepeat(SList(), 5, SList())
+	ConfirmRepeat(SList("A"), 1, SList("A"))
+	ConfirmRepeat(SList("A"), 2, SList("A", "A"))
+	ConfirmRepeat(SList("A"), 3, SList("A", "A", "A"))
+	ConfirmRepeat(SList("A"), 4, SList("A", "A", "A", "A"))
+	ConfirmRepeat(SList("A"), 5, SList("A", "A", "A", "A", "A"))
+}
+
+func TestSSliceCar(t *testing.T) {
+	ConfirmCar := func(s *SSlice, r string) {
+		n := s.Car()
+		if ok := n == r; !ok {
+			t.Fatalf("head should be '%v' but is '%v'", r, n)
+		}
+	}
+	ConfirmCar(SList("A", "B", "C", "D", "E", "F"), "A")
+}
+
+func TestSSliceCdr(t *testing.T) {
+	ConfirmCdr := func(s, r *SSlice) {
+		if n := s.Cdr(); !n.Equal(r) {
+			t.Fatalf("tail should be '%v' but is '%v'", r, n)
+		}
+	}
+	ConfirmCdr(SList("A", "B", "C", "D", "E", "F"), SList("B", "C", "D", "E", "F"))
+}
+
+func TestSSliceRplaca(t *testing.T) {
+	ConfirmRplaca := func(s *SSlice, v interface{}, r *SSlice) {
+		if s.Rplaca(v); !s.Equal(r) {
+			t.Fatalf("slice should be '%v' but is '%v'", r, s)
+		}
+	}
+	ConfirmRplaca(SList("A", "B", "C", "D", "E", "F"), "B", SList("B", "B", "C", "D", "E", "F"))
+}
+
+func TestSSliceRplacd(t *testing.T) {
+	ConfirmRplacd := func(s *SSlice, v interface{}, r *SSlice) {
+		if s.Rplacd(v); !s.Equal(r) {
+			t.Fatalf("slice should be '%v' but is '%v'", r, s)
+		}
+	}
+	ConfirmRplacd(SList("A", "B", "C", "D", "E", "F"), nil, SList("A"))
+	ConfirmRplacd(SList("A", "B", "C", "D", "E", "F"), "B", SList("A", "B"))
+	ConfirmRplacd(SList("A", "B", "C", "D", "E", "F"), SList("F", "E", "D", "C"), SList("A", "F", "E", "D", "C"))
+}
